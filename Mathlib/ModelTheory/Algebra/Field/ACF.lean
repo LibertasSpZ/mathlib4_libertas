418e84ee399ea3ca546e2c0c545381b84c5adcd5
LibertasSpZ <szhu@terpmail.umd.edu>
2024-02-18 16:46:45 -0800

    initial set up of ACF formalization :

---
+++ Mathlib/ModelTheory/Algebra/Field/ACF.lean
@@ -0,0 +1,22 @@
+/-
+TODO: Address Author and License
+-/
+
+import Mathlib.ModelTheory.ElementarySubstructures
+import Mathlib.ModelTheory.Definability
+import Mathlib.ModelTheory.LanguageMap
+import Mathlib.ModelTheory.Algebra.Field.CharP
+
+/-! #First order theory of ACFs
+This file defines the first order theory of algebraically
+closed fields as a theory over the language of rings.
+
+## Main defionitions:
+TODO
+
+## Main theorems:
+TODO
+
+## Main references:
+TODO: Marker.
+-/
